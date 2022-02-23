c462e0e52ec44ac12f958f1f49d68fbea3641548
Sumith Damodaran <sumithpd@gmail.com>
2022-02-23 09:21:54 +0100

    Initial commit from starter kit

---
+++ config.js
@@ -0,0 +1,67 @@
+const config = {
+    gatsby: {
+        pathPrefix: '/',
+        siteUrl: 'https://pomodoro-codelab.netlify.app/',
+        gaTrackingId: null,
+        trailingSlash: false,
+    },
+    header: {
+        logo: 'https://developers.google.com/community/gdg/images/logo-lockup-gdg-horizontal-full.png',
+        logoLink: 'https://pomodoro-codelab.netlify.app/',
+        title: "GDG",
+        githubUrl: 'https://github.com/sumithpdd/flutter_pomodoro_codelab',
+        helpUrl: '',
+        tweetText: '',
+        social: `<li>
+		    <a href="https://twitter.com/sumithpdd" target="_blank" rel="noopener">
+		      <div class="twitterBtn">
+		        <img src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/twitter-brands-block.svg' alt={'Twitter'}/>
+		      </div>
+		    </a>
+		  </li>
+			`,
+        links: [{ text: '', link: '' }],
+        search: {
+            enabled: false,
+            indexName: '',
+            algoliaAppId: process.env.GATSBY_ALGOLIA_APP_ID,
+            algoliaSearchKey: process.env.GATSBY_ALGOLIA_SEARCH_KEY,
+            algoliaAdminKey: process.env.ALGOLIA_ADMIN_KEY,
+        },
+    },
+    sidebar: {
+        forcedNavOrder: [
+            // '/introduction', // add trailing slash if enabled above    
+        ],
+        links: [{ text: 'Sumith Damodaran', link: 'https://Sumithpd.com' }],
+        frontline: false,
+        ignoreIndex: true,
+        title: "<a href='https://developers.google.com/community/gdg'>Pomodoro app codelab</a><div class='greenCircle'></div>",
+    },
+    siteMetadata: {
+        title: 'Pomodoro App Codelab | Sumith Damodaran',
+        description: 'Documentation built with mdx.',
+        ogImage: null,
+        docsLocation: 'https://github.com/sumithpdd/flutter_pomodoro_codelab',
+        favicon: 'https://graphql-engine-cdn.hasura.io/img/hasura_icon_black.svg',
+    },
+    pwa: {
+        enabled: false, // disabling this will also remove the existing service worker.
+        manifest: {
+            name: 'Flutter Pomodoro App Codelab',
+            short_name: 'PomodoroAppCodelab',
+            start_url: '/',
+            background_color: '#6b37bf',
+            theme_color: '#6b37bf',
+            display: 'standalone',
+            crossOrigin: 'use-credentials',
+            icons: [{
+                src: 'src/pwa-512.png',
+                sizes: `512x512`,
+                type: `image/png`,
+            }, ],
+        },
+    },
+};
+
+module.exports = config;
